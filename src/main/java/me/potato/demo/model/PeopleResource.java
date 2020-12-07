a3c378107f48948495a306b697deb3d2ea60aff7
Potato White <bravopotato@gmail.com>
2020-12-07 21:09:59 +0900

    add methodproperties

--- src/main/java/me/potato/demo/model/PeopleResource.java
+++ src/main/java/me/potato/demo/model/PeopleResource.java
@@ -1,8 +1,17 @@
 package me.potato.demo.model;
 
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.MethodProperties;
 import io.quarkus.rest.data.panache.ResourceProperties;
+
+import java.util.List;
 
 @ResourceProperties(path="people", hal=true)
 public interface PeopleResource extends PanacheEntityResource<Person, Long> {
+  @MethodProperties(path="all", exposed=true)
+  List<Person> findAll();
+
+  @MethodProperties(exposed=false)
+  boolean delete(Long id);
+
 }
